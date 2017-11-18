355011ad1467bef24fed987e3dd8e301b2b8f0b3
Olivia Garrett <oliviajonesgarrett@gmail.com>
2017-11-17 19:32:50 -0500

    edits to javascript t

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,6 +10,7 @@ var sapphireValue;
 var diamondValue;
 var wins = 0;
 var losses = 0;
+var totalScore = 0;
 
 //function startGame(){
 //random number generator between 19-120	
@@ -19,20 +20,35 @@ var losses = 0;
 	document.getElementById('losses').innerHTML = 'Losses: ' + losses;
 	document.getElementById('total-score').innerHTML = startingScore;
 
+$(document).ready(function(){
+
 //assign each of the four crystals a random number between 1-12
-	//rubyValue = Math.floor((Math.random() *12) + 1);
+	rubyValue = Math.floor((Math.random() *12) + 1);
 	emeraldValue = Math.floor((Math.random() *12) + 1);
 	sapphireValue = Math.floor((Math.random() *12) + 1);
 	diamondValue = Math.floor((Math.random() *12) + 1);
 
 //on click add a specific amount of points to the total score
-$(document).ready(function(){
 $("#ruby-button").on("click", function() {
-	var  
 	rubyValue = Math.floor((Math.random() * 12) + 1);
-	totalScore = ruby + startingScore;
-	$("#total-score").after(totalScore);
+	totalScore == rubyValue +  startingScore;
+	$("#total-score").text(totalScore);
 });
+
+$('#emerald-button').on('click', function() {
+	//emeraldValue = Math.floor((Math.random() *12) + 1);
+	totalScore == emeraldValue + startingScore;
+	$('#total-score').text(totalScore);
+
+});
+
+$('#sapphire-button').on('click', function() {
+	//sapphireValue = Math.floor((Math.random() *12) + 1);
+	totalScore == sapphireValue + startingScore;
+	$('#total-score').text(totalScore);
+
+});
+
 });
 
 
